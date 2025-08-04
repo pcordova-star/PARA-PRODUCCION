13516ba428236beb92e1b65376909c48f66e4de3
Paulo Córdova <pcordova@woken.cl>
2025-08-04 19:22:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,13 +3,8 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
-    });
+    // Use applicationDefault() to automatically find credentials in a Firebase environment.
+    admin.initializeApp();
   } catch (error: any) {
     console.error('Firebase admin initialization error', error.stack);
   }
